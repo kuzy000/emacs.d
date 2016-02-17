4f8bf5b6b49d029f25aba18a9565d0a5ef021f61
Egor Moiseenko <kuzy000@gmail.com>
2016-02-18 00:31:46 +0300

    Up

--- my/my-use-package.el
+++ my/my-use-package.el
@@ -8,7 +8,10 @@
 (package-initialize)
 
 (when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
   (package-install 'use-package))
+
+(require 'use-package)
 
 (setq use-package-always-ensure t)
 
