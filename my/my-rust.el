c42d193aea4400391d9bf2387b671cf533c7d3ea
Egor Moiseenko <kuzy000@gmail.com>
2016-04-15 18:54:27 +0300

    Up

--- my/my-rust.el
+++ my/my-rust.el
@@ -1,11 +1,15 @@
 (use-package rust-mode
   :config
   (use-package racer
+    :bind ("C-," . racer-find-definition)
     :config
     (add-hook 'rust-mode-hook #'racer-mode)
     (add-hook 'racer-mode-hook #'eldoc-mode)
     (setq racer-cmd "~/.cargo/bin/racer")
     (setq racer-rust-src-path "/usr/src/rust/src")
+
+    ;; (define-key racer-mode-map (kbd "C-,") #'racer-find-definition)
+
     (use-package company
       :config
       (add-hook 'racer-mode-hook #'company-mode))))
