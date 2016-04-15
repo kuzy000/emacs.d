db89d9eebda0d62c0a787b07543efe3d399e5456
Egor Moiseenko <kuzy000@gmail.com>
2016-04-15 18:25:28 +0300

    Fix racer setup

--- my/my-rust.el
+++ my/my-rust.el
@@ -4,6 +4,8 @@
     :config
     (add-hook 'rust-mode-hook #'racer-mode)
     (add-hook 'racer-mode-hook #'eldoc-mode)
+    (setq racer-cmd "~/.cargo/bin/racer")
+    (setq racer-rust-src-path "/usr/src/rust/src")
     (use-package company
       :config
       (add-hook 'racer-mode-hook #'company-mode))))
